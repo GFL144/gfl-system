cf413f388a8fc3fa9fa29708e71b01cadec4b26c
GFL144 <gregstoff3@gmail.com>
2025-10-20 22:21:02 +1000

    CloudSync: GFL-ModuleGuard.ps1

---
+++ scripts/GFL-ModuleGuard.ps1
@@ -0,0 +1,3 @@
+﻿. C:\\GFL-System\\Utils\\GFL-Logging.ps1
+param([string[]]$Modules=@(''BurntToast'',''Pester''))
+foreach($m in $Modules){ try{ if(-not (Get-Module -ListAvailable -Name $m)){ Write-GFLLog "Installing module $m"; Install-Module $m -Scope CurrentUser -Force -AllowClobber -ErrorAction Stop } } catch { Write-GFLLog "Module $m install failed: $($_.Exception.Message)" 'WARN' } }
