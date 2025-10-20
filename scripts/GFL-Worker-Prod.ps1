3ddf42583ed2040de698ffba878aa0a8e104a25b
GFL144 <gregstoff3@gmail.com>
2025-10-20 22:21:32 +1000

    CloudSync: GFL-Worker-Prod.ps1

---
+++ scripts/GFL-Worker-Prod.ps1
@@ -0,0 +1,10 @@
+﻿param([string]$TaskJson)
+$t = $TaskJson | ConvertFrom-Json
+function Ping-Url($u){ try{ (Invoke-WebRequest -UseBasicParsing -Uri $u -TimeoutSec 3).StatusCode }catch{ 0 } }
+switch($t.kind){
+  'http-probe' {
+    $u = $t.args.url
+    @{ url=$u; status=(Ping-Url $u); ts=(Get-Date).ToString('o') } | ConvertTo-Json
+  }
+  default { 'unsupported' }
+}
