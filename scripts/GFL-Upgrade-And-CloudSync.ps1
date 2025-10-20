7bc01a6373d93c4d528f3534e45eaed011825fd0
GFL144 <gregstoff3@gmail.com>
2025-10-20 22:21:29 +1000

    CloudSync: GFL-Upgrade-And-CloudSync.ps1

---
+++ scripts/GFL-Upgrade-And-CloudSync.ps1
@@ -0,0 +1,5 @@
+﻿# Turbo Vox: Upgrade & Cloud Sync
+# (same logic as you just ran interactively)
+# Tip: run as Administrator for upgrades.
+
+<PLACE THE FULL SCRIPT BODY YOU ALREADY PASTED EARLIER HERE>
