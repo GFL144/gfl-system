fa65fa3b7ed4f89fef1851fe3fed536f4fc78d6f
GFL144 <gregstoff3@gmail.com>
2025-10-20 22:21:10 +1000

    CloudSync: GFL-PruneLogs.ps1

---
+++ scripts/GFL-PruneLogs.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference='Stop'
+$Logs = Join-Path 'C:\GFL-System' 'Logs'
+$keep = 30
+$files = Get-ChildItem $Logs -File -EA SilentlyContinue | Sort-Object LastWriteTime -Descending
+if($files.Count -gt $keep){ $files[$keep..($files.Count-1)] | Remove-Item -Force -EA SilentlyContinue }
